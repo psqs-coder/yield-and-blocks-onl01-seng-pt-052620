a41ad66dfa06349054676747881e6b18b2d91106
Bruce Parker <detailsharks@gmail.com>
2020-06-07 19:07:48 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,4 @@
-def hello_t (array)
+def hello_t(array)
    i = 0 
    
    while i < array.length
@@ -7,8 +7,4 @@ def hello_t (array)
  end
 end
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
+
